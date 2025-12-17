31e14e346fa7d12d98f026bdb66633a9f3d69368
alperburaak <alperburaak@outlook.com>
2025-12-17 14:11:25 +0300

    dependency implementation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,6 +39,10 @@ android {
     buildFeatures {
         compose = true
     }
+
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.3.2"
+    }
 }
 
 dependencies {
@@ -60,4 +64,33 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+
+    /* -------------------- COROUTINES + FLOW -------------------- */
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
+
+    /* -------------------- KOIN (DI) -------------------- */
+    implementation("io.insert-koin:koin-android:3.5.6")
+    implementation("io.insert-koin:koin-androidx-compose:3.5.6")
+
+    /* -------------------- RETROFIT (REST API) -------------------- */
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+
+    /* -------------------- OKHTTP -------------------- */
+    implementation("com.squareup.okhttp3:okhttp:4.12.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
+
+    /* -------------------- WEBSOCKET -------------------- */
+    implementation("io.socket:socket.io-client:2.1.0") {
+        exclude(group = "org.json", module = "json")
+    }
+
+    /* -------------------- DATASTORE -------------------- */
+    implementation("androidx.datastore:datastore-preferences:1.1.1")
+
+    /* -------------------- MAPBOX -------------------- */
+    implementation ("com.mapbox.maps:android-ndk27:11.17.1")
+    implementation ("com.mapbox.extension:maps-compose-ndk27:11.17.1")
 }
